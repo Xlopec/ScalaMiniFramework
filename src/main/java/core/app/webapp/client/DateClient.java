ee21e50a34f7dffaf8c5e79d3537c6940fd55fc1
Maksym Oliinyk <maksym.oliinyk@ctdev.io>
2019-08-09 12:27:58 +0300

    fixed scala version

--- src/main/java/core/app/webapp/client/DateClient.java
+++ src/main/java/core/app/webapp/client/DateClient.java
@@ -1,11 +1,11 @@
 package core.app.webapp.client;
 
+import java.io.File;
+import java.io.IOException;
+
 import core.di.BeanContext;
 import core.di.BeanFactory;
 import core.di.imp.XmlBeanContext;
-
-import java.io.File;
-import java.io.IOException;
 
 /**
  * Trivial client for the date server.
@@ -19,7 +19,7 @@ public class DateClient {
      * it serves.
      */
     public static void main(String[] args) throws IOException {
-        final BeanContext context = new XmlBeanContext(new File("/Users/max/IdeaProjects/ScalaMiniFramework/src/main/resources/ServerConfig.xml"));
+        final BeanContext context = new XmlBeanContext(new File("/Users/user/IdeaProjects/ScalaMiniFramework/src/main/resources/ServerConfig.xml"));
         final BeanFactory factory = context.getBeanFactory();
         final Client client = factory.instantiate(Client.class);
 
